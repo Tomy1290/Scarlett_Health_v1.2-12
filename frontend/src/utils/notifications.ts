5609a475554b540fdf4bf7cff31054bc43370f20
Scarlett Health App <scarlett@health.app>
2025-09-14 21:06:15 +0000

    auto-commit for 75247aa9-2bd3-4478-a396-49611afe67ae

--- frontend/src/utils/notifications.ts
+++ frontend/src/utils/notifications.ts
@@ -1,5 +1,6 @@
 import * as Notifications from 'expo-notifications';
 import { Platform, Alert } from 'react-native';
+import * as Device from 'expo-device';
 
 // Configure notification handler to allow all notifications
 Notifications.setNotificationHandler({
